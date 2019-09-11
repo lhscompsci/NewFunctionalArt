b7c63dbe530d7dfd060be944afbff97b75dcaa2d
Black, Samuel Thomas <sam.black@gatech.edu>
2019-09-11 08:53:57 -0500

    Changed things up just slightly

--- MyTurtlePollack.py
+++ MyTurtlePollack.py
@@ -1,6 +1,22 @@
 import turtle
+scn = turtle.Screen()
+scn.bgcolor("yellow")
 
+alice = turtle.Turtle()
+bob = turtle.Turtle()
+cindy = turtle.Turtle()
 
-turtle.forward(200)
+bob.shape("turtle")
+cindy.shape("square")
 
-turtle.exitonclick()
+bob.fillcolor("white")
+
+alice.forward(200)
+bob.right(75)
+bob.forward(150)
+bob.pencolor("blue")
+bob.begin_fill()
+bob.circle(150)
+bob.end_fill()
+
+scn.exitonclick()
